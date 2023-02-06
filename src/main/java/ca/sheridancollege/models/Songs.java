2dda0e13e945c77c04e7f76a78f885841f5c015b
RC C <renante.co@gmail.com>
2023-02-06 18:10:40 -0500

    Html file and basic bean

---
+++ src/main/java/ca/sheridancollege/models/Songs.java
@@ -0,0 +1,17 @@
+package ca.sheridancollege.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Songs {
+    private Long id;
+    private String title;
+    private String artist;
+    private String genre;
+    private final String[] genres = {"Spoken Word", "Electronic", "Classical", "Country"};
+}
